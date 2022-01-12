540702bfdff76bc780b355f9d80acd60b00e0db2
vigneshbellie <vigneshprismsurface@gmail.com>
2022-01-12 11:23:16 +0530

    The task is completed

---
+++ src/org/java/Git.java
@@ -0,0 +1,12 @@
+package org.java;
+
+public class Git {
+
+	public static void main(String[] args) {
+		
+	System.out.println("my fathers name is bellie");
+	System.out.println("my mothers name is manjula");
+	
+	}
+
+}
